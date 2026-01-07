67d3815eddbd25a4f557f1c20ef2dce1a9197d03
leekyoung9x <huytung55123@gmail.com>
2026-01-07 05:13:50 +0200

    feat: Register server repository and service for improved server management

--- GunnyApi/Program.cs
+++ GunnyApi/Program.cs
@@ -56,6 +56,8 @@ builder.Services.AddAuthentication(options =>
 // Register Repositories and Services
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IServerRepository, ServerRepository>();
+builder.Services.AddScoped<IServerService, ServerService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
